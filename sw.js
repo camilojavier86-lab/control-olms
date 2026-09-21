ce486a8ce4b0884fc473fdd01467047cccdc586a
camilojavier86-lab <camilojavier86@gmail.com>
2026-09-21 17:07:47 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const C='control-olms-v6-4';
+const C='control-olms-v6-5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./mambito-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
-  }else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
+      const copy=r.clone();
+      caches.open(C).then(c=>c.put('./index.html',copy));
+      return r;
+    }).catch(()=>caches.match('./index.html')));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
 self.addEventListener('notificationclick',e=>{
   e.notification.close();
