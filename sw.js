1514fb69fd160558d217ab5ad656b53ec5f658c4
camilojavier86-lab <camilojavier86@gmail.com>
2026-09-21 15:56:16 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const C='control-olms-v6-1';
+const C='control-olms-v6-3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./mambito-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
     e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
-  }else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
 self.addEventListener('notificationclick',e=>{
   e.notification.close();
